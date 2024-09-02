e66469bc3c0a88fa839f5bedae2d5128923cf68e
jeresotoo <jere3009@gmail.com>
2024-09-01 23:53:33 -0300

    Add files via upload

--- javascript/index.js
+++ javascript/index.js
@@ -1,27 +1,3 @@
-let nombre;
-let edad;
-let listaNombreDeAlumnos;
-
-nombre = "firu"
-edad = 20
-listaNombreDeAlumnos = ["nicolas", "nana", "firulays"]
-
-
-let  listaDeEdades = [32, 42, 12]
-
-
-let perro = {
-    raza :"colin",
-    edad: 5,
-    velocidad: 15,
-    dieta: ["carne","productos balanceados","leche"]
-}
-
-console.log();
-
-
-
-
 function calcArea (π, radio) {
     π = 3.1415926536
      let area = π * radio * 2;
