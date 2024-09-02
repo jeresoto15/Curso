986e9d86a40b512947cb35cc26b56e0dd6093127
jeresotoo <jere3009@gmail.com>
2024-09-01 23:39:02 -0300

    Add files via upload

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,32 @@
-let mayor = prompt( "Ingrese su edad para continuar")
+let nombre;
+let edad;
+let listaNombreDeAlumnos;
 
-if (mayor >= 18) {
-    alert ("Bienvenido a nuestra pagina")
-} else {
-    alert ("Eres menor, entras bajo tu propio riesgo")
+nombre = "firu"
+edad = 20
+listaNombreDeAlumnos = ["nicolas", "nana", "firulays"]
+
+
+let  listaDeEdades = [32, 42, 12]
+
+
+let perro = {
+    raza :"colin",
+    edad: 5,
+    velocidad: 15,
+    dieta: ["carne","productos balanceados","leche"]
 }
+
+console.log();
+
+
+
+
+function calcArea (π, radio) {
+    π = 3.1415926536
+     let area = π * radio * 2;
+     console.log("el area de un circulo es: " + area)
+}
+
+
+calcArea(1, 2)
