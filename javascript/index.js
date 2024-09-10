47bc7b895bae54ecac9ca93cdb42cea0a3b4d1f6
jeresotoo <jere3009@gmail.com>
2024-09-10 20:16:38 -0300

    Add files via upload

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,55 @@
 function calcArea (π, radio) {
     π = 3.1415926536
-     let area = π * radio * 2;
+    let area = π * radio * 2;
      console.log("el area de un circulo es: " + area)
 }
 
-calcArea()
+calcArea(1,1)
+
+
+
+function cuentaProgresiva(){
+    for(let i = 0; i < 10; i++){
+        console.log(i);
+    }
+}
+
+cuentaProgresiva()
+
+
+function calcularNumeroMayor(numero1,numero2,numero3,numero4){
+    if(numero1 > numero2){
+        return "el numero mayor es: " + numero1
+    } else if(numero2 > numero1){
+        return "el numero mayor es: " + numero2
+    } else if(numero3 > numero4){
+        return "el numero mayor es: " + numero3
+    }
+     else if(numero4 > numero3){
+        return "el numero mayor es: " + numero4
+    }
+}
+
+let numeromayor = calcularNumeroMayor(33, 45, 50, 60)
+
+console.log(numeromayor)
+
+
+
+
+const inputName = document.querySelector("#inp-nombre");
+const outputText = document.querySelector("#salida-saludo");
+function saludar(){
+    const saludoCompleto = "hola, me llamo Jarvis, un placer conocerte " + inputName.value;
+    outputText.innerHTML = saludoCompleto;
+}
+
+
+function saludar(){
+    if (inputName.value != ""){
+        const saludoCompleto = "Hola! , Me llamo Kokó , Un placer conocerte " + inputName.value + ".";
+        outputText.innerHTML = saludoCompleto;
+    } else {
+        outputText.innerHTML = "Por favor, ingresa tu nombre para empezar el saludo."
+    }
+}
